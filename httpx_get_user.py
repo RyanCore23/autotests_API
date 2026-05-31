28d5999c3caae0c7020393bccc1ae6547997a707
RyanCore <darielcore23@gmail.com>
2026-05-31 21:14:54 +0300

    Практикуемся в работе с httpx

---
+++ httpx_get_user.py
@@ -0,0 +1,62 @@
+import httpx
+from tools import fakers
+
+with httpx.Client(base_url="http://127.0.0.1:8000") as client:
+
+    #===================================
+    # Создание пользователя
+    # ===================================
+
+    email = fakers.get_random_email()
+    password = "12345678"
+
+    create_user_payload = {
+            "email": email,
+            "password": password,
+            "lastName": "string",
+            "firstName": "string",
+            "middleName": "string"
+        }
+
+    create_user_response = client.post("/api/v1/users", json = create_user_payload)
+
+    print("===Создаём пользователя===")
+
+    create_user_response_data = create_user_response.json()
+    print(f"Пользователь создан: {create_user_response_data}")
+    print(f"Время использования: {create_user_response.elapsed}")
+
+    # ===================================
+    # Логин
+    # ===================================
+
+    login_payload = {
+        "email": create_user_payload["email"],
+        "password": create_user_payload["password"]
+    }
+
+    login_response = client.post("/api/v1/authentication/login", json=login_payload)
+
+    login_response_data = login_response.json()
+    print(f"Login data: {login_response_data}")
+    print(f"Время использования: {login_response.elapsed}")
+
+    # ===================================
+    # Запрос пользователя
+    # ===================================
+
+    get_user_headers = {
+        "Authorization": f"Bearer {login_response_data['token']['accessToken']}"
+    }
+
+    get_user_response = client.get(
+        f"/api/v1/users/{create_user_response_data['user']['id']}",
+        headers=get_user_headers
+    )
+
+    get_user_response_data = get_user_response.json()
+    print(f"Данные пользователя {get_user_response_data}")
+    print(f"Время использования: {get_user_response.elapsed}")
+
+
+
